8145b8028497c9f76c9e49d826bcd1d1dcf50e55
Michael Aquilina <michaelaquilina@gmail.com>
2014-01-25 00:58:28 +0000

    Working implementation!

--- weasel.py
+++ weasel.py
@@ -11,51 +11,116 @@ import random
 from string import ascii_uppercase
 
 
-def mutation(individual, rate=0.01):
-    for index, _ in enumerate(individual):
-        if random.random() < rate:
-            individual[index] = random.choice(ascii_uppercase)
+def crossover(parent1, parent2):
+    # Uniform crossover
+    child1 = []
+    child2 = []
+
+    for index, _ in enumerate(parent1):
+        if random.random() < 0.5:
+            child1.append(parent1[index])
+            child2.append(parent2[index])
+        else:
+            child1.append(parent2[index])
+            child2.append(parent1[index])
+
+    return child1, child2
+
+
+def mutation(individual):
+    index = random.randint(0, len(individual) - 1)
+    individual[index] = random.choice(ascii_uppercase + ' ')
 
     return individual
 
 
 def fitness(message, target):
     value = 0
-    for i in range(target):
-        value += 1 if message[i] == target[i] else 0
+    for index, actual in enumerate(target):
+        value += 1 if message[index] == actual else 0
 
     return value
 
 
-def evaluate(population):
+def evaluate(population, target):
     fitness_values = []
     for individual in population:
-        fitness_values.append(fitness(individual))
+        fitness_values.append(fitness(individual, target))
 
     return fitness_values
 
 
 # Roulette wheel selection
-def selection(fitness_values):
+def selection(population, fitness_values):
 
-    choice = random.randint(sum(fitness_values))
-    for index, value in enumerate(fitness_values):
-        choice -= value
+    results = []
 
-        if choice < 0:
-            return index
+    while len(results) < 2:
+        choice = random.randint(0, sum(fitness_values))
+        for index, value in enumerate(fitness_values):
+            choice -= value
+
+            if choice < 0:
+                results.append(population[index])
+
+    return results[0], results[1]
+
+
+def argmax(numbers):
+
+    max_index = None
+    max_value = -10000
+    for index, value in enumerate(numbers):
+        if value > max_value:
+            max_value = value
+            max_index = index
+
+    return max_index, max_value
 
 
 if __name__ == '__main__':
 
-    target = 'ME THINKS ITS LIKE A WEASEL'
-    population_size = 10
+    import time
+
+    target = 'ME THINKS IT IS LIKE A WEASEL'
+    population_size = 400  # Larger population sizes seem to be better
+    mutation_rate = 0.15
 
     population = []
     for i in range(population_size):
-        message = ''.join([random.choice(ascii_uppercase) for _ in range(len(target))])
+        message = [random.choice(ascii_uppercase) for _ in range(len(target))]
 
         population.append(message)
 
+    t0 = time.time()
+
+    print 'Target = {}'.format(target)
     while True:
-        fitness_values = evaluate(population)
+        fitness_values = evaluate(population, target)
+
+        max_index, max_value = argmax(fitness_values)
+
+        best = ''.join(population[max_index])
+        print 'Best individual: {} ({})'.format(best, max_value)
+
+        if best == target:
+            break
+
+        new_population = []
+        while len(new_population) < population_size:
+            parent1, parent2 = selection(population, fitness_values)
+
+            child1, child2 = crossover(parent1, parent2)
+
+            if random.random() < mutation_rate:
+                mutation(child1)
+            if random.random() < mutation_rate:
+                mutation(child2)
+
+            new_population.append(child1)
+            if len(new_population) < population_size:
+                new_population.append(child2)
+
+        population = new_population
+
+    print 'Reached target in {}'.format(time.time() - t0)
