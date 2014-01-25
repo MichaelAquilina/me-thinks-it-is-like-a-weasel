237c74a0fc23fbed4c9c486268e94c84dccb60eb
Michael Aquilina <michaelaquilina@gmail.com>
2014-01-25 00:03:03 +0000

    Add initial weasel.py with fitness operator, selection and population evaluation

---
+++ weasel.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python3
+
+"""
+Simple script which generates the phrase 'ME THINKS ITS LIKE A WEASEL' using an extremely simple
+genetic algorithm. This was completely inspired and based off what Richard Dawkin's performs in
+his book 'The Blind Watchmaker' as a way of explaining accumulations of small change through the use
+of cumulative selection.
+"""
+
+import random
+from string import ascii_uppercase
+
+
+def fitness(message, target):
+    value = 0
+    for i in range(target):
+        value += 1 if message[i] == target[i] else 0
+
+    return value
+
+
+def evaluate(population):
+    fitness_values = []
+    for individual in population:
+        fitness_values.append(fitness(individual))
+
+    return fitness_values
+
+
+# Roulette wheel selection
+def selection(fitness_values):
+
+    choice = random.randint(sum(fitness_values))
+    for index, value in enumerate(fitness_values):
+        choice -= value
+
+        if choice < 0:
+            return index
+
+
+if __name__ == '__main__':
+
+    target = 'ME THINKS ITS LIKE A WEASEL'
+    population_size = 10
+
+    population = []
+    for i in range(population_size):
+        message = ''.join([random.choice(ascii_uppercase) for _ in range(len(target))])
+
+        population.append(message)
